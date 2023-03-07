4e3698cc279d37326fee647a030914a0dbd04ddb
Preslav Getov <preslav.getov@yara.com>
2023-03-07 15:23:56 +0200

    end of lesson

--- examples/example.ts
+++ examples/example.ts
@@ -1,6 +1,40 @@
-let myName:string = 'PG'
+let myName = 'PG'
 
 // myName = 2 WILL NOT WORK!
 
 let myNum:number = 2222.3333
 
+function sum(a:number,b:number){
+    return a+b
+}
+
+//myNum = 'hey'
+//will inferr result type based on arg types 
+const result = sum(4,3)
+
+
+const numbers = [1,2,3,4]
+
+let item:(number|string) = 'hey'
+item = 4
+
+interface Person{
+    name: string
+}
+
+function returnGreeting(person: Person){
+    return person.name
+}
+type idName = 'streamer'|'worker'
+
+interface Worker{
+    name: idName
+}
+
+console.log(returnGreeting({
+    name: "Mie"
+}))
+
+let nameOfPerson = 'mike'
+
+//const myExampleObject:Worker = {name : "hired worker"}
