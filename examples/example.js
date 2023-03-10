e3f5a99559c1341da64049ba3e4672416ef077ff
Preslav Getov <preslav.getov@yara.com>
2023-03-10 17:57:42 +0200

    adding a tuple

--- examples/example.js
+++ examples/example.js
@@ -1,55 +1,31 @@
 // let myName = "PG";
-
 // // myName = 2 WILL NOT WORK!
-
 // let myNum: number = 2222.3333;
-
 // function sum(a: number, b: number) {
 //   return a + b;
 // }
-
 // //myNum = 'hey'
 // //will inferr result type based on arg types
 // const result = sum(4, 3);
-
 // const numbers = [1, 2, 3, 4];
-
 // let item: number | string = "hey";
 // item = 4;
-
 // interface Person {
 //   name: string;
 // }
-
 // function returnGreeting(person: Person) {
 //   return person.name;
 // }
 // type idName = "streamer" | "worker";
-
 // interface Worker {
 //   name: idName;
 // }
-
 // console.log(
 //   returnGreeting({
 //     name: "Mie",
 //   })
 // );
-
 // let nameOfPerson = "mike";
-
 //const myExampleObject:Worker = {name : "hired worker"}
-
-// let reg: RegExp = /\S/i;
-// console.log("hey".match(reg));
-
-let singularTuple: [string, number, boolean] = ["rick", 1234, false];
-
-type Guitarist = {
-  fullName: string;
-  monicker?: string;
-};
-
-let evh: Guitarist = {
-  fullName: "Eddie Van Halen",
-};
+var reg = /\S/i;
+console.log("hey".match(reg));
